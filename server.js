6eb05af189a9795a2c14f0108dee09576051cf72
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-11-29 10:50:23 -0700

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData = [
     {
         name: 'Mark'
@@ -24,6 +27,29 @@ app.get('/users/:id', function(req,res){
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login', function (req,res){
+    //Typically passwords are encrupted using something like bcrypt before sending to database
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if(username===mockUsername && password === mockPassword){
+        //In practice, use JSON web token sign method here to make an encrypted token
+        res.json({
+            success: true,
+            message: 'password and username math!',
+            tocken: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success : false,
+            message: 'password or username do not match '
+        })
+    }
 })
 
 app.listen(8000, function(){
